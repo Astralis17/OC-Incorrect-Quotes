01269705c8d6e2d9e8cc786d32876263410ca71e
Astralis17 <aurora.spark17@gmail.com>
2024-05-19 12:03:08 +0100

    Update main.py
    Added more quotes

--- main.py
+++ main.py
@@ -98,13 +98,16 @@ Quotes= [
              [P1, " to ", P2, ": You're gonna get yourself killed one day, but I'll be darned if I don't get a chance to watch it happen"],
              [P1, ": Why do you even bother saving my stupid neck?", "&", P2, ": I can't let you get yourself killed. Who'd pay back all the favours you owe me?"],
              [P1, ": Do you think I could leave some money for the people to buy a replacement?", "&", P2, ": You'll be giving somebody money for something, that's for sure."],
-             [P1, ": Wetter than a selkie strip club out there!", "&", P2, ": I- I don't know how to respond to that."],    
+             [P1, ": Wetter than a selkie strip club out there!", "&", P2, ": I- I don't know how to respond to that."],
+             [P1, ": Gods, you're impatient.", "&", P2, ": Not impatient. Hungry.", "&", P1, ": Hunger just means your body's impatient for food.", "&", P2, ": Bullshit, hunger means... well... uhh.... Hunger means I'm going to drag you out by the ear if you don't come on!", "&", P1, ": You're truly a master of philosophy."],
+             [P1, ": I'm not a very 'jump right in' kind of person.", "&", P2, ": Well then show them that trick where you make things burst into flames. Kids'll love that.", "&", P1, ": ...I'll do that. Thank you, ", P2, "."],
              ],
         
             [#Three Character Quotes
              [P1, ": I swear, by the summer's end, thy will have taken thee to at least one party, ", P2, "! 'tis my mission for this summer!", "&", P2, ": ...still bitter that they chose ", P3, " for the school play, are we?", "&", P1, ": Shut up- you and I both know that the only reason that, that thing got the lead role of Juliette is because she was blowing the drama teacher"],
              [P1, ": What's going on?", "&", P2, " chuckling nervously: I don't know what you're talking about. Everything is completely normal and ", P3, " is exactly where they're supposed to be", "&", P1, ": What do you mean? WHERE IS ", P3CAP],
              [P1, ": I get your point. But gods beyond, ", P3, ", has anyone ever told you that you're just really mean?", "&", P2,": At least a few times a day. You learn to translate it out after a while. After that, they make decent sense."],
+             [P1, ": Sometimes I hope God is real, but then I see ", P2, " and my hopes are immediately shattered.", "&", P3, ": Hey! You can't just-", "&", P2, ", eating burnt toast: No, no, they're right."],
             ],
         
             [#Four Character Quotes
